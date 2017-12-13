df0837aa77667dcb2b64bbc40924345c59ef014d
jonyzhao <j.c.zhao@foxmail.com>
2017-12-13 16:27:56 +0800

    singleton

---
+++ Afflatus/Assets/Modules/Utils/CsSingleton.cs
@@ -0,0 +1,64 @@
+﻿namespace Afflatus
+{
+    /// <summary>
+    /// c#单例模版类，通过管理器显式创建、销毁。
+    /// </summary>
+    public class CsSingleton<T> where T : class, new()
+    {
+
+        private static T _instance;
+
+        protected CsSingleton(){ }
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    CreateInstance();
+                }
+
+                return _instance;
+            }
+
+            protected set
+            {
+                _instance = value;
+            }
+        }
+
+
+        // 创建单例
+        public static void CreateInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new T();
+
+                (_instance as CsSingleton<T>).Init();
+            }
+        }
+
+
+        // 删除单例
+        public static void DestroyInstance()
+        {
+            if (_instance != null)
+            {
+                (_instance as CsSingleton<T>).UnInit();
+                _instance = null;
+            }
+        }
+        
+
+        public virtual void Init()
+        {
+        }
+
+
+        public virtual void UnInit()
+        {
+        }
+    }
+}
---
+++ Afflatus/Assets/Modules/Utils/MonoSingleton.cs
@@ -0,0 +1,106 @@
+﻿namespace Afflatus
+{
+    using System;
+    using UnityEngine;
+
+    /// <summary>
+    /// monobehaviour单例。跨场景不销毁，生命周期最好由管理器控制。
+    /// </summary>
+    public class MonoSingleton<T> : MonoBehaviour where T : Component
+    {
+        private static Component _instance;
+        private static object _lock = new object();
+
+        public static T Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    CreateInstance();
+                }
+                return (T)_instance;
+            }
+        }
+
+        public static void CreateInstance()
+        {
+            lock(_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = (T)FindObjectOfType(typeof(T));
+
+                    if (FindObjectsOfType(typeof(T)).Length > 1)
+                    {
+                        Debug.LogError("CreateInstance: More than 1 monobehaviour singleton found. Check & Reopening the scene might fix it.");
+                    }
+
+                    if (_instance == null)
+                    {
+                        GameObject go = new GameObject();
+                        go.name = typeof(T).ToString() +"(AutoCreate)";
+                        DontDestroyOnLoad(go);
+
+                        _instance = go.AddComponent<T>();
+
+                    }
+                }
+            }
+        }
+
+        public static void DestroyInstance()
+        {
+            if (_instance != null)
+            {
+                MonoBehaviour.Destroy(_instance.gameObject);
+            }
+
+            _instance = null;
+        }
+
+
+        private void Awake()
+        {
+            if (_instance != null && _instance.gameObject != gameObject) //确保单件实例的唯一性
+            {
+                MonoBehaviour.Destroy(gameObject);
+
+            }
+            else if(_instance == null)
+            {
+                lock (_lock)
+                {
+                    _instance = this;
+                    DontDestroyOnLoad(gameObject);
+                    Debug.Log(typeof(T).ToString() + "awake init");
+
+                }
+            }
+            
+        }
+
+
+        private void OnDestroy()
+        {
+            if (_instance != null && _instance.gameObject == this.gameObject)
+            {
+                _instance = null; //确保单件的静态实例会随着GameObject销毁
+                UnInit();
+            }
+        }
+        
+       
+
+        protected virtual void Init()
+        {
+
+        }
+
+        protected virtual void UnInit()
+        {
+            
+        }
+    }
+
+}
